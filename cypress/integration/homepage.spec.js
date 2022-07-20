d567c0acb31f5b402a1f06764543eef231b0f31f
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-20 13:50:27 -0400

    Pass test user should see res cards

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -5,9 +5,13 @@ describe("Homepage flow", () => {
 
   it("A user can visit the app", () => {
     cy.contains("Turing Cafe Reservations")
-    .get("form")
-    .contains("Make Reservation")
+    .get("form").contains("Make Reservation")
   });
 
+  it("A user should see all existing reservations", () => {
+    cy.get("div").should("have.class", "resy-container")
+    .get("div").should("have.class", "card-wrapper")
+    .get("h2").should("have.class", "name")
+  });
 
 });
