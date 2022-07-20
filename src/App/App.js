10c0e2122d34d3d25f3ad787c1c124504d4b99bc
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-20 11:41:30 -0400

    Add GET request in componentDidMount

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: [],
+      error: ""
+    };
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:3001/api/v1/reservations")
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log("Error");
+          return this.setState({error: "Error, please try again"})
+        }
+      })
+      .then(data => this.setState({reservations: data}))
+      .catch(err => {
+        console.log("Error");
+        return this.setState({error: "Error, please try again"})
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +35,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
