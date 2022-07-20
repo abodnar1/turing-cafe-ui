d84f1e9bbef64067b9c70a3e0ac5ad358c3fa23c
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-20 12:57:57 -0400

    Add inputs to Form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,12 +4,47 @@ import "./Form.css";
 class Form extends Component {
   constructor() {
     super()
-    this.state = {};
+    this.state = {
+      name: "",
+      date: "",
+      time: "",
+      numGuests: ""
+    };
   }
 
   render() {
     return (
-      <h1>Hi form!</h1>
+      <form className="form-container">
+        <input
+          type="text"
+          name="name"
+          placehold="Name"
+          value={this.state.name}
+        />
+
+        <input
+          type="date"
+          name="date"
+          placehold="Date (mm/dd)"
+          value={this.state.date}
+        />
+
+        <input
+          type="text"
+          name="time"
+          placehold="Time"
+          value={this.state.time}
+        />
+
+        <input
+          type="number"
+          name="numGuests"
+          placehold="Number of guests"
+          value={this.state.numGuests}
+        />
+
+        <button className="make-res-button-">Make Reservation</button>
+      </form>
     )
   }
 };
